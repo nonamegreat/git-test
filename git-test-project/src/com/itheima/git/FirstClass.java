83e7eac350583fa7e97584e1e388eb703c8ca95c
zixu <243750382@qq.com>
2020-06-26 10:46:31 +0800

    把本地工程添加到git本地仓库中

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,17 @@
+package com.itheima.git;
+
+/**
+ * @author Mr.chen
+ * @description
+ * @date 2020/6/26 10:23
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
